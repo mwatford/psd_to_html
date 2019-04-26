9baf4ffa4df74d9388f0fc643c66adcfb22ec439
Peupty <melvillewatford@gmail.com>
2019-04-26 18:51:22 +0200

    added sidebar containing projects routing

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
   <div id="app">
-    <appNav></appNav>
+    <appNav v-if="appState.appNav"></appNav>
     <router-view/>
   </div>
 </template>
 
 <script>
-import appNav from './appNav.vue'
+import appNav from "./appNav.vue";
 export default {
   components: {
-    appNav,
+    appNav
   },
   data() {
-    return {};
+    return {
+      appState: {
+        appNav: true
+      }
+    };
+  },
+  created() {
+    eventBus.$on("toggleNav", () => {
+      this.appState.appNav = !this.appState.appNav;
+    });
   }
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Mina");
 body {
   background-color: #f7f7f7;
   margin: 0;
   overflow-y: scroll;
   font-family: "Oswald", sans-serif;
-  // width: 100vw;
 }
 #app {
   position: relative;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
--- src/appNav.vue
+++ src/appNav.vue
@@ -1,22 +1,65 @@
 <template>
   <nav class="nav">
-    asd
+    <div class="nav__hide" @click="toggleNav">HIDE</div>
+    <router-link v-for="project in routes" :key="project.name" :to="project.path">{{ project.name }}</router-link>
   </nav>
 </template>
 
 <script>
+import routes from "./routes.js";
+import { toggleNav } from "./mixins.js";
 export default {
-
-}
+  mixins: [toggleNav],
+  data() {
+    return {
+      routes
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+* {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+}
 .nav {
-  position: absolute;
+  font-family: "Mina", sans-serif;
+  position: fixed;
   top: 0;
   left: 0;
   width: 300px;
-  height: 1000px;
+  height: 100%;
   background: #fff;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border-right: 2px solid black;
+  z-index: 2;
+
+  &__hide {
+    width: 100%;
+    height: 100px;
+    background: burlywood;
+  }
+
+  & a {
+    text-decoration: none;
+    margin: 5px 0 0 2em;
+    font-size: 20px;
+    font-weight: bold;
+    &:active,
+    &:visited {
+      color: inherit;
+    }
+    &:hover {
+      color: #b32929;
+    }
+    &.router-link-exact-active {
+      color: #b32929;
+    }
+  }
 }
 </style>
