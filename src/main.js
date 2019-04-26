9984ec7972f766a45a1cb1b6118d9e3e9b21a380
Peupty <melvillewatford@gmail.com>
2019-04-26 18:55:00 +0200

    created global eventbus

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import Routes from './routes.js'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import Routes from './routes.js';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
+
+window.eventBus = new Vue();
 
 const router = new VueRouter({
   routes: Routes,
@@ -16,4 +18,4 @@ new Vue({
   el: '#app',
   render: h => h(App),
   router
-})
+});
