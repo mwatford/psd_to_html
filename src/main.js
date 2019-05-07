88ebdee691492cd47ebc57a0b97c7012355f613e
Peupty <melvillewatford@gmail.com>
2019-05-07 21:35:33 +0200

    added font awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import Routes from './routes.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCoffee, faAngleLeft, faAngleRight);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
