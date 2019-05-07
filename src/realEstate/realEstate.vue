6f14dd98cfb3c9f1f7d1126ab970b80b693258d8
Peupty <melvillewatford@gmail.com>
2019-05-07 21:42:34 +0200

    Revert "Revert "added signIn and signUp modals with behaviour""
    This reverts commit f97d71d53af7953e9d03435c8eb5ab53bcc7850d.

--- src/realEstate/realEstate.vue
+++ src/realEstate/realEstate.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="realEstate">
     <appNav></appNav>
-    <div class="banner">
-      <h1 class="banner__title">Affordable Homes</h1>
-      <div>
-        <input class="banner__search" type="text"/>
-        <input type="submit" value="Search" class="banner__submit">
-      </div>
-      <h2 class="banner__subtitle">houses you can afford</h2>
-    </div>
+    <sideBar></sideBar>
+    <banner></banner>
   </div>
 </template>
 
 <script>
 import appNav from "./appNav.vue";
+import banner from "./banner.vue";
+import sideBar from "./sideBar.vue";
 export default {
   components: {
-    appNav
+    appNav,
+    banner,
+    sideBar
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Iceland");
 * {
   display: flex;
   justify-content: center;
@@ -35,40 +34,5 @@ export default {
   width: 100%;
   min-height: 100vh;
   flex-direction: column;
-}
-.banner {
-  background: url("./assets/banner.jpg");
-  background-size: cover;
-  background-blend-mode: color-burn;
-  background-color: rgba(103, 81, 53, 0.52);
-  background-position: 50% 70%;
-  color: white;
-  height: calc(100vh - 80px);
-  flex-direction: column;
-  justify-content: space-around;
-
-  &__title {
-    font-size: 114px;
-    font-family: 'AR BONNIE', sans-serif;
-  }
-  &__search {
-    height: 45px;
-    max-width: 600px;
-    width: 60%;
-    border: 2px solid #88F169;
-    background: rgba(255, 255,255, 0.7);
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    padding: 0 1em;
-    font-size: 20px;
-  }
-  &__submit {
-    height: 45px;
-    width: 120px;
-    background: #88F169;
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-    border: none;
-  }
 }
 </style>
