fe3ef4139135b93f1f20fce4dbc584029413908f
Peupty <melvillewatford@gmail.com>
2019-05-22 12:18:11 +0200

    commenting

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Marble from './marble/Marble.vue';
 import realEstate from './realEstate/realEstate.vue';
 import Home from './Home.vue'
 
+//add new routes to the end
 export default [
   { path: '/', component: Home, name: 'Home' },
   { path: '/Marble', component: Marble, name: 'Marble' },
