5de4857956702528cf441ee6f2cd662bf4b13d79
Peupty <melvillewatford@gmail.com>
2019-05-23 01:03:20 +0200

    formating and styles changes

--- src/marble/components/Home.vue
+++ src/marble/components/Home.vue
@@ -16,7 +16,7 @@
       </div>
     </div>
     <div class="contentTwo">
-      <h3 class="contentTwo__header">Latest</h3>
+      <h3 class="contentTwo__header">OUR LATEST WORKS</h3>
       <div class="contentTwo__post" v-for="(post, index) in posts" :key="index">
         <picture>
           <img :src="post.img" alt="#">
@@ -38,155 +38,155 @@
 export default {
   data() {
     return {
-      title1: 'We are a digital & branding agency based in London.',
-      title2: 'We love to turn great ideas into beautiful products.',
-      quote: 'Cras nattus consectetur purus sit amet fementum. \
+      title1: "We are a digital & branding agency based in London.",
+      title2: "We love to turn great ideas into beautiful products.",
+      quote:
+        "Cras nattus consectetur purus sit amet fementum. \
         Donec id elit non mi porta gravida at eget metus. Duis mollis,\
-        est non commodo luctus erat porttitor.',
+        est non commodo luctus erat porttitor.",
       news: [
         {
           id: 1,
           img: require(`../assets/img/icon1.png`),
-          title: 'Tristiquet',
-          text: 'Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
-            ut fermentum massa justo risus.'
+          title: "Tristiquet",
+          text:
+            "Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
+            ut fermentum massa justo risus."
         },
         {
           id: 2,
           img: require(`../assets/img/icon1.png`),
-          title: 'Tristiquet',
-          text: 'Fusce depibis, moje commodore, tortor mauris condimentum nibh,\
-            ut fermentum massa justo risus.'
+          title: "Tristiquet",
+          text:
+            "Fusce depibis, moje commodore, tortor mauris condimentum nibh,\
+            ut fermentum massa justo risus."
         },
         {
           id: 3,
           img: require(`../assets/img/icon1.png`),
-          title: 'Tristiquet',
-          text: 'Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
-            ut fermentum massa justo risus.'
+          title: "Tristiquet",
+          text:
+            "Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
+            ut fermentum massa justo risus."
         },
         {
           id: 4,
-          img: require('../assets/img/icon1.png'),
-          title: 'Tristiquet',
-          text: 'Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
-            ut fermentum massa justo risus.'
+          img: require("../assets/img/icon1.png"),
+          title: "Tristiquet",
+          text:
+            "Fusce depibis, cursus commodo, tortor mauris condimentum nibh,\
+            ut fermentum massa justo risus."
         }
       ],
-      placeholderText: 'Cras nattus consectetur purus sit amet fementum.\
+      placeholderText:
+        "Cras nattus consectetur purus sit amet fementum.\
 				  Donec id elit non mi porta gravida at eget metus. Duis mollis,\
-          est non commodo luctus erat porttitor.',
+          est non commodo luctus erat porttitor.",
       posts: [
-        { img: require('../assets/img/post1.png') },
-        { img: require('../assets/img/post1.png') },
-        { img: require('../assets/img/post1.png') },
+        { img: require("../assets/img/post1.png") },
+        { img: require("../assets/img/post1.png") },
+        { img: require("../assets/img/post1.png") }
       ]
-    }
-  },
-}
+    };
+  }
+};
 </script>
 
 <style lang="scss">
 .slider {
-    grid-column: 1 / span 5;
-    grid-row: 1;
-    display: flex;
-    background: url('../assets/img/slider1.png') no-repeat;
-    background-position: center center;
-    background-size: cover;
+  grid-column: 1 / span 5;
+  grid-row: 1;
+  display: flex;
+  background: url("../assets/img/slider1.png") no-repeat;
+  background-position: center center;
+  background-size: cover;
 
-    @media (max-width: 50em) {
-      background-size: fill;
-      font-size: 0.8em;
+  @media (max-width: 50em) {
+    background-size: fill;
+    font-size: 0.8em;
+  }
+
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: "Roboto Slab", serif;
+
+  &__h1 {
+    align-self: center;
+    color: #f3f3f3;
+  }
+
+  &__h2 {
+    align-self: center;
+    color: #a8a8a8;
+  }
+
+  &__button {
+    background-color: #3f8dbf;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    width: 150px;
+    height: 35px;
+    text-transform: uppercase;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 2px;
+    align-self: center;
+    margin-top: 1.8vw;
+
+    &:hover {
+      cursor: pointer;
     }
-
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    font-family: 'Roboto Slab', serif;
-
-    &__h1 {
-        align-self: center;
-        color: #f3f3f3;
-    }
-
-    &__h2 {
-        align-self: center;
-        color: #a8a8a8;
-    }
-
-    &__button {
-        background-color: #3f8dbf;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        width: 150px;
-        height: 35px;
-        text-transform: uppercase;
-        font-size: 11px;
-        font-weight: bold;
-        letter-spacing: 2px;
-        align-self: center;
-        margin-top: 1.8vw;
-
-        &:hover {
-            cursor: pointer;
-        }
-    }
-
+  }
 }
 .contentOne__icon {
-    align-content: center;
-    border-bottom: 1px solid #d7d7d7;
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    padding: 20% 10%;
-    text-align: center;
+  align-content: center;
+  border-bottom: 1px solid #d7d7d7;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  padding: 20% 10%;
+  text-align: center;
 }
 .contentTwo__header {
-    grid-column: 1 / span 3;
-    grid-row: 1;
-    text-transform: uppercase;
-    padding: 0;
-    // padding: auto 0;
-    display: flex;
-    align-self: center;
+  grid-column: 1 / span 3;
+  grid-row: 1;
+  text-transform: uppercase;
+  padding: 0;
+  display: flex;
+  align-self: center;
 
-    @media (max-width: 50em) {
-      grid-column: 1;
-    }
+  @media (max-width: 50em) {
+    grid-column: 1;
+  }
 }
 .contentTwo__post {
-    font-size: 0.9em;
-    display: flex;
-    flex-flow: column;
-    align-content: center;
-    // justify-content: center;
+  font-size: 0.9em;
+  display: flex;
+  flex-flow: column;
+  align-content: center;
 
-    picture img {
-        height: auto;
-        width: 100%;
-    }
-
+  picture img {
+    height: auto;
+    width: 100%;
+  }
 }
 .quote {
-    display: flex;
-    flex-flow: column;
-    background-color: #efefef;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-flow: column;
+  background-color: #efefef;
+  align-items: center;
+  justify-content: space-around;
+  padding: 40px 0;
 
-    &__sign {
-        margin: 0 auto;
-    }
-
-    &__text {
-        padding: 0 30%;
-        color: #a1a1a1;
-        font-family: 'Bitter', serif;
-        text-align: center;
-    }
+  &__text {
+    width: 80%;
+    max-width: 600px;
+    color: #a1a1a1;
+    font-family: "Bitter", serif;
+    text-align: center;
+  }
 }
 </style>
